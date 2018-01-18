c931f5aa20410e93551d39c183703b4b925a02e9
KnowledgeSchool <35336506+KnowledgeSchool@users.noreply.github.com>
2018-01-18 16:35:35 +0530

    Add files via upload
    Address and Values Limits and Fragmentation Concepts

---
+++ address_limits.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("=======================================\n");
+    printf(" Address and Limits of Variables \n");
+    printf("=======================================\n");
+
+    int number = 10;
+    char character = 128;
+    int number2 = 12;
+
+    printf(" Display the address of character: 0x%x \n",&character);
+    printf(" Display the address of number: 0x%x \n",&number);
+    printf(" Display the address of number2: 0x%x \n",&number2);
+
+    printf(" Value of character = %d \n",character);
+    printf(" Value of number = %d \n",number);
+    printf(" Value of number2 = %d \n",number2);
+
+    return 0;
+}
+
